3060b2c6cfe7a5c2df3e7994a86ca5d42998e6eb
sohakhan <sohakhan@khi.iba.edu.pk>
2018-04-29 11:38:57 +0500

    Home

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index(){
-        $title = 'Welcome To Laravel!';
+        $title = 'Welcome To Peach Blossom!';
         //return view('pages.index', compact('title'));
         return view('pages.index')->with('title', $title);
     }
