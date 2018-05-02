053cd90ffeb2d2830d4f39169f2d080f60f2e775
Hassan <saeedhassan@outlook.com>
2018-05-02 06:13:50 +0500

    added post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,7 @@
             <div class="well">
                 <div class="row">
                     <div class="col-md-4 col-sm-4">
-                        <img style="width:100%" src="/{{$post->cover_image}}">
+                        <img style="width:50%" src="/{{$post->cover_image}}">
                     </div>
                     <div class="col-md-8 col-sm-8">
                         <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
